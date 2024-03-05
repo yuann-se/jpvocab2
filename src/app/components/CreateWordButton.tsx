23e25accb30dc9acea49080fd01ef6c183289d5a
yuann-se <yuann96@yandex.ru>
2024-03-05 15:51:26 +0300

    create button: keep relative position on window resize

--- src/app/components/CreateWordButton.tsx
+++ src/app/components/CreateWordButton.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useEffect, useRef, useState } from "react"
-import { IconButton } from "@mui/material"
+import { Fade, IconButton } from "@mui/material"
 import AddCircleRoundedIcon from '@mui/icons-material/AddCircleRounded';
 import WordDialog from "./WordDialog/WordDialog"
 import Draggable, { DraggableEventHandler } from "react-draggable";
@@ -9,12 +9,25 @@ import { usePreferencesContext } from "./providers/PreferencesProvider";
 
 const CreateWordButton = () => {
     const isDragging = useRef<boolean>(false)
+    const [resizeKey, setResizeKey] = useState(0)
 
     // required to remove react-draggable warnings
     const buttonRef = useRef<HTMLButtonElement>(null)
     const [isDialogOpen, setIsDialogOpen] = useState(false)
 
     const { preferences, updatePreferences } = usePreferencesContext()
+
+    useEffect(() => {
+        const handleResize = () => {
+            setResizeKey(prev => prev + 1)
+        }
+        window.addEventListener("resize", handleResize)
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, [])
+
 
     const handleClick = () => {
         if (!isDragging.current) {
@@ -35,11 +48,10 @@ const CreateWordButton = () => {
 
     if (!preferences) return null
 
-    console.log(preferences)
-
     return (
         <>
             <Draggable
+                key={resizeKey}
                 defaultPosition={{
                     x: window.innerWidth * preferences.createButtonPosition.x,
                     y: window.innerHeight * preferences.createButtonPosition.y
@@ -51,13 +63,15 @@ const CreateWordButton = () => {
                 bounds={'#homePage'}
                 nodeRef={buttonRef}
             >
-                <IconButton
-                    className="createWordButton"
-                    onClick={handleClick}
-                    ref={buttonRef}
-                >
-                    <AddCircleRoundedIcon />
-                </IconButton>
+                <Fade in={!!preferences} className="createWordButton">
+                    <IconButton
+                        className="createWordButton"
+                        onClick={handleClick}
+                        ref={buttonRef}
+                    >
+                        <AddCircleRoundedIcon />
+                    </IconButton>
+                </Fade>
             </Draggable>
 
             <WordDialog
